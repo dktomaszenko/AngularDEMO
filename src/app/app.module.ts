9b753ce50cc8c31ab6008d65fc0cd154d7006c1c
dktomaszenko <xjxxwbm9fx>
2017-10-13 11:22:13 +0200

    lo-dash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoDashComponent } from './components/src/app/components/lo-dash/lo-dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoDashComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/lo-dash/lo-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lo-dash',
+  templateUrl: './lo-dash.component.html',
+  styleUrls: ['./lo-dash.component.css']
+})
+export class LoDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
